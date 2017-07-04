254c2819351a10806f0729692b44752ec447d60b
Monir Hossain <mknishad1@ymail.com>
2017-07-04 15:06:54 +0600

    Done with StudentPortalFragment

--- app/src/main/java/com/ideafactorybd/nubstudentportal/NUBWebsiteFragment.java
+++ app/src/main/java/com/ideafactorybd/nubstudentportal/NUBWebsiteFragment.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-
 public class NUBWebsiteFragment extends Fragment {
+
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,6 @@ public class NUBWebsiteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nubwebsite, container, false);
+        return view = inflater.inflate(R.layout.fragment_nubwebsite, container, false);
     }
 }
--- app/src/main/java/com/ideafactorybd/nubstudentportal/StudentPortalFragment.java
+++ app/src/main/java/com/ideafactorybd/nubstudentportal/StudentPortalFragment.java
@@ -1,13 +1,23 @@
 package com.ideafactorybd.nubstudentportal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class StudentPortalFragment extends Fragment {
+
+    View view;
+    WebView studentPortalWebview;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,32 @@ public class StudentPortalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_portal, container, false);
+        return view = inflater.inflate(R.layout.fragment_student_portal, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        studentPortalWebview = (WebView) view.findViewById(R.id.webview_student_portal);
+        WebSettings webSettings = studentPortalWebview.getSettings();
+        studentPortalWebview.loadUrl("http://182.160.97.196:8088/studentportal/");
+        webSettings.setJavaScriptEnabled(true);
+        studentPortalWebview.setWebViewClient(new MyWebViewClient());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.e("StudentPortalFragment", Uri.parse(url).getHost());
+            if (Uri.parse(url).getHost().equals("182.160.97.196")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
