cd8aba3872879b438a0eb6684132450ef4d5c3c9
Monir Hossain <mknishad1@ymail.com>
2017-07-06 16:35:57 +0600

    Manage adView in life cycle methods

--- app/src/main/java/com/ideafactorybd/nubstudentportal/MainActivity.java
+++ app/src/main/java/com/ideafactorybd/nubstudentportal/MainActivity.java
@@ -135,4 +135,28 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
 }
